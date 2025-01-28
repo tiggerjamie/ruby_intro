b7e47c429f2b642eef93598012e9890593a46c85
Tigran Tyler Pell <tigran.tylerpell@kellogg.northwestern.edu>
2025-01-28 21:08:06 +0000

    hash prac

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,25 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+me = {name: "Tigran", "location" => "Evanston", "status" => "alive"}
+
+puts me
 
 # Accessing data from the hash
 
+puts me[:name]
+location = me["location"]
+puts location   
+puts me["status"]
+
 # More Complex Hashes
+
+my_profile = {
+    name: {first: "tiggy", last: "piggy"},
+    "status" => "DOA",
+    location: {city: "Evanston", state: "IL"}
+}
+
+puts my_profile
+puts my_profile["status"]
+puts my_profile[:location][:city] + my_profile[:location][:state]
